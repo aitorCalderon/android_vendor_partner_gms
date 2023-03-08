52460668c26add570bc16c4d680f9d92a7b6b6de
Aitor Calderon <a.calderon@gmx.net>
2023-03-08 16:57:35 +0100

    Add library dependencies for AuroraStore

--- AuroraStore/Android.mk
+++ AuroraStore/Android.mk
@@ -6,4 +6,9 @@ LOCAL_SRC_FILES := com.aurora.store_41.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
+
+ifeq (true,$(call math_gt_or_eq,$(PLATFORM_VERSION),12))
+  LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
+
 include $(BUILD_PREBUILT)
